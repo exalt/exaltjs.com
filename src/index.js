d78721c97836d810c6219c4bf8e1333971e85906
jleeson <jleeson2000@gmail.com>
2021-09-17 13:52:41 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { Component, html } from "@exalt/core";
+import { define } from "@exalt/core/decorators";
+import "./index.css";
+
+@define("app-root")
+export class App extends Component {
+
+    render() {
+        return html`
+            <h1>Hello World!</h1>
+        `;
+    }
+}
